2e86f15df3550308f42a69b880305885375f9688
Ilham Berlian O <70630655+IlhamBerlianO@users.noreply.github.com>
2024-05-10 18:05:25 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -44,16 +44,29 @@ st.write(f'Harga Saham: ${prices[index]}')
 st.write(f'Deskripsi Perusahaan:')
 st.write(descriptions[index])
 
-# Fungsi untuk mengonversi teks menjadi suara
+# Fungsi untuk merubah bahasa untuk deskripsi perusahaan
 def text_to_speech(text):
     tts = gTTS(text=text, lang='en')
     speech = io.BytesIO()
     tts.write_to_fp(speech)
     return speech.getvalue()
 
+# Tambahkan tombol untuk merubah bahasa untuk deskripsi perusahaan
+if st.button("Translate ke Indonesia"):
+    # Translate deskripsi dari bahasa Inggris ke bahasa Indonesia
+    descriptions_id = [translator.translate(desc, src='en', dest='id').text for desc in descriptions_en]
+    # Tampilkan deskripsi perusahaan yang telah diterjemahkan
+    st.write(f'Company Description (Indonesian):')
+    st.write(descriptions_id[index])
+
 # Tambahkan tombol untuk membaca deskripsi perusahaan
 if st.button("Baca Deskripsi"):
-    speech_bytes = text_to_speech(descriptions[index])
+    # Jika deskripsi dalam bahasa Inggris
+    if "descriptions_id" not in locals():
+        speech_bytes = text_to_speech(descriptions_en[index])
+    # Jika deskripsi telah diterjemahkan ke bahasa Indonesia
+    else:
+        speech_bytes = text_to_speech(descriptions_id[index], lang='id')
     st.audio(speech_bytes, format='audio/mp3')
 
 st.write(f'Created by Ilham Berlian Oktavio')
