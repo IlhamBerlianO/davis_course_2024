3b28357190c34b8937188c5cc1e44645c909956e
Ilham Berlian O <70630655+IlhamBerlianO@users.noreply.github.com>
2024-05-10 18:51:18 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,6 +1,8 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 import pandas as pd
 from gtts import gTTS
+from deep_translator import GoogleTranslator
 import io
 import base64
 
@@ -18,34 +20,44 @@ companies = baca['Company'].tolist()
 prices = baca['Price'].tolist()
 descriptions = baca['Description'].tolist()
 
-# Plot grafik
-st.bar_chart(baca.set_index('Company')['Price'])
+# Inisialisasi objek Translator
+translator = GoogleTranslator(source='en', target='id')
+
+# # Plot grafik
+plt.figure(figsize=(10, 6))
+plt.barh(companies, prices, color='#398bff')
+plt.xlabel('Harga Saham ($)')
+plt.ylabel('Perusahaan')
+plt.title('Grafik Saham Perusahaan 2024')
+plt.gca().invert_yaxis() 
+st.pyplot(plt)
 
 # Judul aplikasi
-st.title('Company Profile')
+st.title('Profil Perusahaan')
 
 # Pilih saham dari dropdown
-selected_stock = st.selectbox('Choose Company:', companies)
+selected_stock = st.selectbox('Pilih Perusahaan:', companies)
 
 # Temukan indeks saham yang dipilih di daftar perusahaan
 index = companies.index(selected_stock)
 
-# Tampilkan detail perusahaan yang dipilih dalam bahasa Inggris
-st.write(f'Company name: {companies[index]}')
-st.write(f'Stock Price: ${prices[index]}')
-st.write(f'Company Description (English):')
+# Tampilkan detail perusahaan yang dipilih
+st.write(f'Nama Perusahaan: {companies[index]}')
+st.write(f'Harga Saham: ${prices[index]}')
+st.write(f'Deskripsi Perusahaan:')
 st.write(descriptions[index])
 
-# Fungsi untuk mengonversi teks menjadi suara
-def text_to_speech(text, lang='id'):
-    tts = gTTS(text=text, lang=lang)
-    speech = io.BytesIO()
-    tts.write_to_fp(speech)
-    return speech.getvalue()
+# # Tambahkan tombol untuk merubah bahasa untuk deskripsi perusahaan
+# if st.button("Translate ke Indonesia"):
+#     # Translate deskripsi dari bahasa Inggris ke bahasa Indonesia
+#     description_id = translator.translate(descriptions[index])
+#     # Tampilkan deskripsi perusahaan yang telah diterjemahkan
+#     st.write(f'Company Description (Indonesian):')
+#     st.write(description_id)
 
 # Tambahkan tombol untuk membaca deskripsi perusahaan
 if st.button("Baca Deskripsi"):
-    speech_bytes = text_to_speech(descriptions[index])
+    speech_bytes = text_to_speech(descriptions_id[index])
     st.audio(speech_bytes, format='audio/mp3')
-
+    
 st.write(f'Created by Ilham Berlian Oktavio')
